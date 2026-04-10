8d0bc5db93536eb449c0b0e06526ed9f0fdf07b4
Thiago Abreu <tabreu.br@gmail.com>
2026-04-10 16:02:38 -0300

    config: add environment and docker settings

---
+++ src/main/java/com/github/tabreubr/musiclass/MusiclassApplication.java
@@ -0,0 +1,13 @@
+package com.github.tabreubr.musiclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusiclassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusiclassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/tabreubr/musiclass/MusiclassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tabreubr.musiclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusiclassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
